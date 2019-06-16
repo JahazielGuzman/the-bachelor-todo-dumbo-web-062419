01667de5564528602880449008b5d8a844469b91
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-15 22:48:19 -0400

    still need to finish average age for season

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,54 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  winner = data[season].find{|contestant| contestant["status"] ==  "Winner"}
+  winner["name"].split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  name = ""
+  data.each do |season, list|
+  	list.each do |contestant|
+  		if contestant["occupation"] == occupation
+  			name = contestant["name"]
+  			break
+  		end
+  	end
+  	if name != ""
+  		break
+  	end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
+	num_con = 0
   # code here
+  data.each do |season, list|
+  	list.each do |contestant|
+  		if contestant["hometown"] == hometown
+  			num_con += 1
+  		end
+  	end
+  end
+  num_con
 end
 
 def get_occupation(data, hometown)
   # code here
+  occupation = ""
+  data.each do |season, list|
+  	list.each do |contestant|
+  		if contestant["hometown"] == hometown
+  			occupation = contestant["occupation"]
+  			break
+  		end
+  	end
+  	if occupation != ""
+  		break
+  	end
+  end
+  occupation
 end
 
 def get_average_age_for_season(data, season)
